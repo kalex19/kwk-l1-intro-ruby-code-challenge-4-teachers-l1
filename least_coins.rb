18427082e8e79fe8e7b017cd12b9f9a966562ffd
kalex19 <klewis@rockymountainprep.org>
2018-07-12 13:28:05 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,9 +1,13 @@
 def least_coins(cents)
-  coins = {:quaters => 0, :dimes => 0, nickels => 0 , penny => 0}
+  coins = {:quaters => 0, :dimes => 0, :nickels => 0 , :penny => 0}
 
 
 while cents >= 25 
   coins [:quarters] += 1 
   cents -= 25 
-  
+end
+
+
+
+
  
